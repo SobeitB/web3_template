0172e66df2f668bea468100d6f251eaeedbadd28
panda.code.lab@gmail.com <panda.code.lab@gmail.com>
2023-08-16 18:56:41 +0300

    console errors fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   reactStrictMode: true,
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals = ['pino-pretty', 'lokijs', 'encoding'];
     return config;
   }
 };
